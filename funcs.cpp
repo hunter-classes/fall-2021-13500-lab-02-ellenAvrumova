f25f9992483499f97f0147a3845de1291e0e2f89
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-09-11 13:03:18 -0400

    updated main and header file

--- funcs.cpp
+++ funcs.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include "funcs.h"
 
 void print_interval(int L, int U) {
-    std::cout << "Please enter L: " << L;
-    std::cout << "Please enter U: " << U;
+    std::cout << "Please enter L: " << L << std::endl;
+    std::cout << "Please enter U: " << U << std::endl;
     
     while(L < U) {
         std::cout << L << " ";
         L++;
     }
+    std::cout << std::endl;
     
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "funcs.h"
 
 int main() {
+    print_interval(-5,10);
+    print_interval(12,28);
+    print_interval(3,4);
+    print_interval(-10,-2);
 
     return 0;
 }
